3974cb338c3a968abfda7b065e9394d743aea110
sajid-techis <sajid@techis.io>
2022-08-19 22:49:20 +0530

    Updated files

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Client,
+  CTA,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.boxWidth.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Client />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Client from "./Client";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export { Navbar, Billing, CardDeal, Business,Client,CTA,FeedbackCard,Footer,GetStarted,Hero,Stats,Testimonials};
